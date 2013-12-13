f85637bb1980e00aecfde903af076a6f9a36fc1b
Michael Anstis <michael.anstis@gmail.com>
2013-12-13 14:31:47 +0000

    DROOLS-365 Rule Templates and BRL Fragments are inconsistent with Decision Tables. Initial fix for ConnectiveConstraints

--- drools-workbench-models/drools-workbench-models-guided-template/src/test/java/org/drools/workbench/models/guided/template/backend/RuleTemplateModelDRLPersistenceTest.java
+++ drools-workbench-models/drools-workbench-models-guided-template/src/test/java/org/drools/workbench/models/guided/template/backend/RuleTemplateModelDRLPersistenceTest.java
@@ -16,10 +16,8 @@
 
 package org.drools.workbench.models.guided.template.backend;
 
-import org.drools.workbench.models.commons.backend.rule.RuleModelDRLPersistenceImpl;
 import org.drools.workbench.models.commons.backend.rule.RuleModelPersistence;
 import org.drools.workbench.models.datamodel.oracle.DataType;
-import org.drools.workbench.models.datamodel.rule.ActionInsertFact;
 import org.drools.workbench.models.datamodel.rule.BaseSingleFieldConstraint;
 import org.drools.workbench.models.datamodel.rule.CompositeFieldConstraint;
 import org.drools.workbench.models.datamodel.rule.ConnectiveConstraint;
@@ -275,6 +273,270 @@ public class RuleTemplateModelDRLPersistenceTest {
                 "dialect \"mvel\"\n" +
                 "when\n" +
                 "Person( field2 == \"bar\" )\n" +
+                "then\n" +
+                "end\n";
+
+        m.addRow( new String[]{ null, "bar" } );
+
+        checkMarshall( expected,
+                       m );
+    }
+
+    @Test
+    public void testCompositeConstraintWithConnectiveConstraintBothValues() {
+        TemplateModel m = new TemplateModel();
+        m.name = "t1";
+
+        FactPattern p1 = new FactPattern( "Person" );
+        m.addLhsItem( p1 );
+
+        CompositeFieldConstraint comp = new CompositeFieldConstraint();
+        comp.setCompositeJunctionType( CompositeFieldConstraint.COMPOSITE_TYPE_OR );
+        p1.addConstraint( comp );
+
+        final SingleFieldConstraint X = new SingleFieldConstraint();
+        X.setFieldName( "field1" );
+        X.setFieldType( DataType.TYPE_STRING );
+        X.setConstraintValueType( SingleFieldConstraint.TYPE_TEMPLATE );
+        X.setValue( "$f1" );
+        X.setOperator( "==" );
+        comp.addConstraint( X );
+
+        ConnectiveConstraint connective = new ConnectiveConstraint();
+        connective.setConstraintValueType( BaseSingleFieldConstraint.TYPE_LITERAL );
+        connective.setFieldType( DataType.TYPE_STRING );
+        connective.setOperator( "|| ==" );
+        connective.setValue( "goo" );
+
+        X.setConnectives( new ConnectiveConstraint[ 1 ] );
+        X.getConnectives()[ 0 ] = connective;
+
+        final SingleFieldConstraint Y = new SingleFieldConstraint();
+        Y.setFieldName( "field2" );
+        Y.setFieldType( DataType.TYPE_STRING );
+        Y.setConstraintValueType( SingleFieldConstraint.TYPE_TEMPLATE );
+        Y.setValue( "$f2" );
+        Y.setOperator( "==" );
+        comp.addConstraint( Y );
+
+        String expected = "rule \"t1_0\"\n" +
+                "dialect \"mvel\"\n" +
+                "when\n" +
+                "Person( field1 == \"foo\" || == \"goo\" || field2 == \"bar\" )\n" +
+                "then\n" +
+                "end\n";
+
+        m.addRow( new String[]{ "foo", "bar" } );
+
+        checkMarshall( expected,
+                       m );
+    }
+
+    @Test
+    public void testCompositeConstraintWithConnectiveConstraintFirstValue() {
+        TemplateModel m = new TemplateModel();
+        m.name = "t1";
+
+        FactPattern p1 = new FactPattern( "Person" );
+        m.addLhsItem( p1 );
+
+        CompositeFieldConstraint comp = new CompositeFieldConstraint();
+        comp.setCompositeJunctionType( CompositeFieldConstraint.COMPOSITE_TYPE_OR );
+        p1.addConstraint( comp );
+
+        final SingleFieldConstraint X = new SingleFieldConstraint();
+        X.setFieldName( "field1" );
+        X.setFieldType( DataType.TYPE_STRING );
+        X.setConstraintValueType( SingleFieldConstraint.TYPE_TEMPLATE );
+        X.setValue( "$f1" );
+        X.setOperator( "==" );
+        comp.addConstraint( X );
+
+        ConnectiveConstraint connective = new ConnectiveConstraint();
+        connective.setConstraintValueType( BaseSingleFieldConstraint.TYPE_LITERAL );
+        connective.setFieldType( DataType.TYPE_STRING );
+        connective.setOperator( "|| ==" );
+        connective.setValue( "goo" );
+
+        X.setConnectives( new ConnectiveConstraint[ 1 ] );
+        X.getConnectives()[ 0 ] = connective;
+
+        final SingleFieldConstraint Y = new SingleFieldConstraint();
+        Y.setFieldName( "field2" );
+        Y.setFieldType( DataType.TYPE_STRING );
+        Y.setConstraintValueType( SingleFieldConstraint.TYPE_TEMPLATE );
+        Y.setValue( "$f2" );
+        Y.setOperator( "==" );
+        comp.addConstraint( Y );
+
+        String expected = "rule \"t1_0\"\n" +
+                "dialect \"mvel\"\n" +
+                "when\n" +
+                "Person( field1 == \"foo\" || == \"goo\" )\n" +
+                "then\n" +
+                "end\n";
+
+        m.addRow( new String[]{ "foo", null } );
+
+        checkMarshall( expected,
+                       m );
+    }
+
+    @Test
+    public void testCompositeConstraintWithConnectiveConstraintSecondValue() {
+        TemplateModel m = new TemplateModel();
+        m.name = "t1";
+
+        FactPattern p1 = new FactPattern( "Person" );
+        m.addLhsItem( p1 );
+
+        CompositeFieldConstraint comp = new CompositeFieldConstraint();
+        comp.setCompositeJunctionType( CompositeFieldConstraint.COMPOSITE_TYPE_OR );
+        p1.addConstraint( comp );
+
+        final SingleFieldConstraint X = new SingleFieldConstraint();
+        X.setFieldName( "field1" );
+        X.setFieldType( DataType.TYPE_STRING );
+        X.setConstraintValueType( SingleFieldConstraint.TYPE_TEMPLATE );
+        X.setValue( "$f1" );
+        X.setOperator( "==" );
+        comp.addConstraint( X );
+
+        ConnectiveConstraint connective = new ConnectiveConstraint();
+        connective.setConstraintValueType( BaseSingleFieldConstraint.TYPE_LITERAL );
+        connective.setFieldType( DataType.TYPE_STRING );
+        connective.setOperator( "|| ==" );
+        connective.setValue( "goo" );
+
+        X.setConnectives( new ConnectiveConstraint[ 1 ] );
+        X.getConnectives()[ 0 ] = connective;
+
+        final SingleFieldConstraint Y = new SingleFieldConstraint();
+        Y.setFieldName( "field2" );
+        Y.setFieldType( DataType.TYPE_STRING );
+        Y.setConstraintValueType( SingleFieldConstraint.TYPE_TEMPLATE );
+        Y.setValue( "$f2" );
+        Y.setOperator( "==" );
+        comp.addConstraint( Y );
+
+        String expected = "rule \"t1_0\"\n" +
+                "dialect \"mvel\"\n" +
+                "when\n" +
+                "Person( field2 == \"bar\" )\n" +
+                "then\n" +
+                "end\n";
+
+        m.addRow( new String[]{ null, "bar" } );
+
+        checkMarshall( expected,
+                       m );
+    }
+
+    @Test
+    public void testConnectiveConstraintBothValues() {
+        TemplateModel m = new TemplateModel();
+        m.name = "t1";
+
+        FactPattern p1 = new FactPattern( "Person" );
+        m.addLhsItem( p1 );
+
+        final SingleFieldConstraint X = new SingleFieldConstraint();
+        X.setFieldName( "field1" );
+        X.setFieldType( DataType.TYPE_STRING );
+        X.setConstraintValueType( SingleFieldConstraint.TYPE_TEMPLATE );
+        X.setValue( "$f1" );
+        X.setOperator( "==" );
+        p1.addConstraint( X );
+
+        ConnectiveConstraint connective = new ConnectiveConstraint();
+        connective.setConstraintValueType( BaseSingleFieldConstraint.TYPE_TEMPLATE );
+        connective.setFieldType( DataType.TYPE_STRING );
+        connective.setOperator( "|| ==" );
+        connective.setValue( "goo" );
+
+        X.setConnectives( new ConnectiveConstraint[ 1 ] );
+        X.getConnectives()[ 0 ] = connective;
+
+        String expected = "rule \"t1_0\"\n" +
+                "dialect \"mvel\"\n" +
+                "when\n" +
+                "Person( field1 == \"foo\" || == \"bar\" )\n" +
+                "then\n" +
+                "end\n";
+
+        m.addRow( new String[]{ "foo", "bar" } );
+
+        checkMarshall( expected,
+                       m );
+    }
+
+    @Test
+    public void testConnectiveConstraintFirstValue() {
+        TemplateModel m = new TemplateModel();
+        m.name = "t1";
+
+        FactPattern p1 = new FactPattern( "Person" );
+        m.addLhsItem( p1 );
+
+        final SingleFieldConstraint X = new SingleFieldConstraint();
+        X.setFieldName( "field1" );
+        X.setFieldType( DataType.TYPE_STRING );
+        X.setConstraintValueType( SingleFieldConstraint.TYPE_TEMPLATE );
+        X.setValue( "$f1" );
+        X.setOperator( "==" );
+        p1.addConstraint( X );
+
+        ConnectiveConstraint connective = new ConnectiveConstraint();
+        connective.setConstraintValueType( BaseSingleFieldConstraint.TYPE_TEMPLATE );
+        connective.setFieldType( DataType.TYPE_STRING );
+        connective.setOperator( "|| ==" );
+        connective.setValue( "$f2" );
+
+        X.setConnectives( new ConnectiveConstraint[ 1 ] );
+        X.getConnectives()[ 0 ] = connective;
+
+        String expected = "rule \"t1_0\"\n" +
+                "dialect \"mvel\"\n" +
+                "when\n" +
+                "Person( field1 == \"foo\" )\n" +
+                "then\n" +
+                "end\n";
+
+        m.addRow( new String[]{ "foo", null } );
+
+        checkMarshall( expected,
+                       m );
+    }
+
+    @Test
+    public void testConnectiveConstraintSecondValue() {
+        TemplateModel m = new TemplateModel();
+        m.name = "t1";
+
+        FactPattern p1 = new FactPattern( "Person" );
+        m.addLhsItem( p1 );
+
+        final SingleFieldConstraint X = new SingleFieldConstraint();
+        X.setFieldName( "field1" );
+        X.setFieldType( DataType.TYPE_STRING );
+        X.setConstraintValueType( SingleFieldConstraint.TYPE_TEMPLATE );
+        X.setValue( "$f1" );
+        X.setOperator( "==" );
+        p1.addConstraint( X );
+
+        ConnectiveConstraint connective = new ConnectiveConstraint();
+        connective.setConstraintValueType( BaseSingleFieldConstraint.TYPE_TEMPLATE );
+        connective.setFieldType( DataType.TYPE_STRING );
+        connective.setOperator( "|| ==" );
+        connective.setValue( "goo" );
+
+        X.setConnectives( new ConnectiveConstraint[ 1 ] );
+        X.getConnectives()[ 0 ] = connective;
+
+        String expected = "rule \"t1_0\"\n" +
+                "dialect \"mvel\"\n" +
+                "when\n" +
+                "Person(  )\n" +
                 "then\n" +
                 "end\n";
 
